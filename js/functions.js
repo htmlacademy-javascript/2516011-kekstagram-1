63e296e8f5ae16d09f93c352c5369ae4ffe92db0
bertruchio <mr.2003.artem@gmail.com>
2024-05-02 18:53:40 +0300

    Добавлена функция извлекающая цифры от 0 до 9 из строки

--- js/functions.js
+++ js/functions.js
@@ -11,10 +11,38 @@ function isPalindrome(str) {
   return true;
 }
 
+function extractDigits(input) {
+  if (typeof input === 'string') {
+    let digits = '';
+
+    for (let i = 0; i < input.length; i++) {
+      if (!isNaN(parseInt(input[i], 10))) {
+        digits += input[i];
+      }
+    }
+
+    return digits ? parseInt(digits, 10) : NaN;
+  } else if (typeof input === 'number') {
+    return extractDigits(input.toString());
+  } else {
+    return NaN;
+  }
+}
+
 isPalindrome('топот');
 isPalindrome('ДовОд');
 isPalindrome('Кекс');
 isPalindrome('Лёша на полке клопа нашёл ');
+
+extractDigits('2023 год'); // 2023
+extractDigits('ECMAScript 2022'); // 2022
+extractDigits('1 кефир, 0.5 батона'); // 105
+extractDigits('агент 007'); // 7
+extractDigits('а я томат'); // NaN
+extractDigits(2023); // 2023
+extractDigits(-1); // 1
+extractDigits(1.5); // 15
+
 
 // // Тесты: Функция 1
 // // Строка является палиндромом
@@ -25,3 +53,13 @@ isPalindrome('Лёша на полке клопа нашёл ');
 // console.log(isPalindrome('Кекс')); // false
 // // Это палиндром
 // console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
+
+// // Тесты: Функция 2
+// console.log(extractDigits('2023 год')); // 2023
+// console.log(extractDigits('ECMAScript 2022')); // 2022
+// console.log(extractDigits('1 кефир, 0.5 батона')); // 105
+// console.log(extractDigits('агент 007')); // 7
+// console.log(extractDigits('а я томат')); // NaN
+// console.log(extractDigits(2023)); // 2023
+// console.log(extractDigits(-1)); // 1
+// console.log(extractDigits(1.5)); // 15
