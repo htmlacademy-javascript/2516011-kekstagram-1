5d23cdf3b43229b775916b7ffed5fa704eb74b88
bertruchio <mr.2003.artem@gmail.com>
2024-05-02 23:14:15 +0300

    Добавлена функция для проверки длины строки с максимальным значением

--- js/functions.js
+++ js/functions.js
@@ -45,49 +45,36 @@ function PadString(str, minLength, padChars) {
   return paddedChars + str;
 }
 
-isPalindrome('топот');
-isPalindrome('ДовОд');
-isPalindrome('Кекс');
-isPalindrome('Лёша на полке клопа нашёл ');
+const checkMaxStringLength = (str, maxLength) => str.length <= maxLength;
 
-extractDigits('2023 год'); // 2023
-extractDigits('ECMAScript 2022'); // 2022
-extractDigits('1 кефир, 0.5 батона'); // 105
-extractDigits('агент 007'); // 7
-extractDigits('а я томат'); // NaN
-extractDigits(2023); // 2023
-extractDigits(-1); // 1
-extractDigits(1.5); // 15
+// Тесты: Функция 1
+// Строка является палиндромом
+window.console.log(isPalindrome('топот')); // true
+// Несмотря на разный регистр, тоже палиндром
+window.console.log(isPalindrome('ДовОд')); // true
+// Это не палиндром
+window.console.log(isPalindrome('Кекс')); // false
+// Это палиндром
+window.console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
 
-PadString('1', 2, '0'); // '01'
-PadString('1', 4, '0'); // '0001'
-PadString('q', 4, 'werty'); // 'werq'
-PadString('q', 4, 'we'); // 'wweq'
-PadString('qwerty', 4, '0'); // 'qwerty'
+// Тесты: Функция 2
+window.console.log(extractDigits('2023 год')); // 2023
+window.console.log(extractDigits('ECMAScript 2022')); // 2022
+window.console.log(extractDigits('1 кефир, 0.5 батона')); // 105
+window.console.log(extractDigits('агент 007')); // 7
+window.console.log(extractDigits('а я томат')); // NaN
+window.console.log(extractDigits(2023)); // 2023
+window.console.log(extractDigits(-1)); // 1
+window.console.log(extractDigits(1.5)); // 15
 
-// // Тесты: Функция 1
-// // Строка является палиндромом
-// console.log(isPalindrome('топот')); // true
-// // Несмотря на разный регистр, тоже палиндром
-// console.log(isPalindrome('ДовОд')); // true
-// // Это не палиндром
-// console.log(isPalindrome('Кекс')); // false
-// // Это палиндром
-// console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
+// Тесты: Функция 3
+window.console.log(PadString('1', 2, '0')); // '01'
+window.console.log(PadString('1', 4, '0')); // '0001'
+window.console.log(PadString('q', 4, 'werty')); // 'werq'
+window.console.log(PadString('q', 4, 'we')); // 'wweq'
+window.console.log(PadString('qwerty', 4, '0')); // 'qwerty'
 
-// // Тесты: Функция 2
-// console.log(extractDigits('2023 год')); // 2023
-// console.log(extractDigits('ECMAScript 2022')); // 2022
-// console.log(extractDigits('1 кефир, 0.5 батона')); // 105
-// console.log(extractDigits('агент 007')); // 7
-// console.log(extractDigits('а я томат')); // NaN
-// console.log(extractDigits(2023)); // 2023
-// console.log(extractDigits(-1)); // 1
-// console.log(extractDigits(1.5)); // 15
-
-// // Тесты: Функция 3
-// console.log(PadString('1', 2, '0')); // '01'
-// console.log(PadString('1', 4, '0')); // '0001'
-// console.log(PadString('q', 4, 'werty')); // 'werq'
-// console.log(PadString('q', 4, 'we')); // 'wweq'
-// console.log(PadString('qwerty', 4, '0')); // 'qwerty'
+// Тесты: Функция 4
+window.console.log(checkMaxStringLength('проверяемая строка', 20)); // true
+window.console.log(checkMaxStringLength('проверяемая строка', 18)); // true
+window.console.log(checkMaxStringLength('проверяемая строка', 10)); // false
