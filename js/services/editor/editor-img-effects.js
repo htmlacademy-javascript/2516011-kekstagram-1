8a64cc99b306a7ee1de632429f12f14b665584e4
bertruchio <mr.2003.artem@gmail.com>
2024-07-24 00:36:16 +0300

    feat: добавляет модуль наложения фильтров на изображение в редакторе

---
+++ js/services/editor/editor-img-effects.js
@@ -0,0 +1,77 @@
+const effectsButtons = document.querySelector('.effects');
+const effectLevelSlider = document.querySelector('.effect-level__slider');
+const effectLevelValue = document.querySelector('.effect-level__value');
+const imgUploadPreview = document.querySelector('.img-upload__preview img');
+const effectLevelContainer = document.querySelector('.img-upload__effect-level');
+
+const EFFECTS = {
+  'none': { class: '', filter: '', unit: '', min: 0, max: 100, step: 1 },
+  'chrome': { class: 'effects__preview--chrome', filter: 'grayscale', unit: '', min: 0, max: 1, step: 0.1 },
+  'sepia': { class: 'effects__preview--sepia', filter: 'sepia', unit: '', min: 0, max: 1, step: 0.1 },
+  'marvin': { class: 'effects__preview--marvin', filter: 'invert', unit: '%', min: 0, max: 100, step: 1 },
+  'phobos': { class: 'effects__preview--phobos', filter: 'blur', unit: 'px', min: 0, max: 3, step: 0.1 },
+  'heat': { class: 'effects__preview--heat', filter: 'brightness', unit: '', min: 1, max: 3, step: 0.1 }
+};
+
+const DEFAULT_EFFECT = 'none';
+
+let currentEffect = DEFAULT_EFFECT;
+
+const updateSlider = (effect) => {
+  if (effect === 'none') {
+    effectLevelContainer.classList.add('hidden');
+    imgUploadPreview.style.filter = '';
+    imgUploadPreview.className = 'effects__preview--none';
+    effectLevelValue.value = '';
+    return;
+  }
+  effectLevelContainer.classList.remove('hidden');
+  const { min, max, step } = EFFECTS[effect];
+  effectLevelSlider.noUiSlider.updateOptions({
+    range: {
+      min: min,
+      max: max,
+    },
+    start: max,
+    step: step,
+  });
+};
+
+const resetEffects = () => updateSlider(DEFAULT_EFFECT);
+
+const applyEffect = (effect, value) => {
+  if (effect === 'none') {
+    imgUploadPreview.style.filter = '';
+    return;
+  }
+  const { filter, unit } = EFFECTS[effect];
+  imgUploadPreview.style.filter = `${filter}(${value}${unit})`;
+};
+
+noUiSlider.create(effectLevelSlider, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
+
+const onEffectLevelUpdate = (values, handle) => {
+  const value = values[handle];
+  effectLevelValue.value = value;
+  applyEffect(currentEffect, value);
+};
+
+const onEffectChange = (event) => {
+  const effect = event.target.value;
+  imgUploadPreview.className = `effects__preview--${effect}`;
+  currentEffect = effect;
+  updateSlider(effect);
+};
+
+effectLevelSlider.noUiSlider.on('update', onEffectLevelUpdate);
+effectsButtons.addEventListener('change', onEffectChange);
+
+export { resetEffects };
