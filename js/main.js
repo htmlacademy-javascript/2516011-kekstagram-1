33737f32b9ff02ffe5002ae47c8a7f7c407b9ef7
bertruchio <mr.2003.artem@gmail.com>
2024-07-24 00:33:37 +0300

    fix: переносит скрипты в отдельную папку для редактора

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { generatePhotos } from './data/photos.js';
 import { renderGallery } from './services/gallery.js';
-import './services/editor-loader.js';
+import './services/editor/editor-loader.js';
 
 renderGallery(generatePhotos());
