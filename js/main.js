d4b4936f196f0c9009f8d79582d1b984b2dd0ec6
bertruchio <mr.2003.artem@gmail.com>
2024-08-02 01:39:23 +0300

    feat: подключает новый модуль фильтра фотографий

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 import { renderGallery } from './services/gallery.js';
 import { getData } from './data/api.js';
+import { setupFilters } from './services/filters.js';
 import { showError } from './utils/error-messages.js';
 import { setOnSubmit } from './services/editor/editor-loader.js';
+import { debounce } from './utils/optimization.js';
 
 getData()
   .then((photos) => {
-    renderGallery(photos);
+    const debouncedRenderGallery = debounce(renderGallery);
+    setupFilters(photos, debouncedRenderGallery);
   })
   .catch(
     (err) => {
