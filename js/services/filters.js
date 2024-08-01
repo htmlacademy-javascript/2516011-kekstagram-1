b7afe70d49d5223056daa538940748932a5c0ec1
bertruchio <mr.2003.artem@gmail.com>
2024-08-02 01:29:57 +0300

    feat: добавляет модуль фильтров для фотографий в галерее

---
+++ js/services/filters.js
@@ -0,0 +1,38 @@
+import { sortRandomly, sortByComments } from '../utils/random.js';
+
+const PICTURES_RENDER_COUNT = 10;
+const PhotoFilterSettings = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed'
+};
+
+const imgFilters = document.querySelector('.img-filters');
+
+const filterPhotos = (photos, filterType) => {
+  switch (filterType) {
+    case PhotoFilterSettings.RANDOM:
+      return photos.slice().sort(sortRandomly).slice(0, PICTURES_RENDER_COUNT);
+    case PhotoFilterSettings.DISCUSSED:
+      return photos.slice().sort(sortByComments);
+    default:
+      return photos.slice();
+  }
+};
+
+const setupFilters = (photos, cb) => {
+  Object.values(PhotoFilterSettings).forEach((filter) => {
+    const filterButton = document.getElementById(filter);
+    filterButton.addEventListener('click', () => {
+      document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+      filterButton.classList.add('img-filters__button--active');
+      cb(filterPhotos(photos, filter));
+    });
+  });
+
+  cb(filterPhotos(photos, PhotoFilterSettings.DEFAULT));
+
+  imgFilters.classList.remove('img-filters--inactive');
+};
+
+export { setupFilters, filterPhotos };
